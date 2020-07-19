81589e42da2c98457fc79c3a255ecab1978048db
yuchenduan <116866341+yuchenduan@users.noreply.github.com>
2020-07-19 20:42:16 +0000

    New translations en.json (Korean) [ci skip] Signed-off-by: Björn Kimminich <bjoern.kimminich@gmx.de>

---
+++ app/assets/javascripts/alert.js
@@ -0,0 +1,12 @@
+
+
+// jquery
+// document.getElementById('alert').style.display = 'none'
+// document.getElementById('notice').style.display = 'none'
+//   console.log("tarace marche");
+
+
+ setTimeout(function(){
+document.getElementById('al').style.display = 'none'
+document.getElementById('note').style.display = 'none'
+}, 3000);
