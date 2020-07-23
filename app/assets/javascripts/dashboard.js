66872749c0e0053b05e665c4fb525a4fe5576a45
yuchenduan <116866341+yuchenduan@users.noreply.github.com>
2020-07-23 13:30:06 +0000

    timeout fix

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -8,4 +8,26 @@
      });
      row1.parentNode.parentNode.style.height = "63vh";
      row1.parentNode.parentNode.children[0].style.height = "30%";
-     row1.parentNode.parentNode.children[1].style.displa
+     row1.parentNode.parentNode.children[1].style.display = "flex";
+     const tradingviewid = row1.parentNode.parentNode.children[1].querySelector(".chart").id;
+     new TradingView.widget(
+     {
+      "autosize": true,
+      "symbol": "BINANCE:"+tradingviewid+"USD",
+      "interval": "30",
+      "timezone": "Etc/UTC",
+      "theme": "Dark",
+      "style": "1",
+      "locale": "en",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "allow_symbol_change": true,
+      "no_referral_id": true,
+      "container_id": tradingviewid
+    }
+    );
+   }else {
+    row1.parentNode.parentNode.style.height = "10vh";
+    row1.parentNode.parentNode.children[0].style.height = "100%";
+    row1.parentNode.parentNode.children[1].style.display = "none";
+  
