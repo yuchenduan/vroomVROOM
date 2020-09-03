3f1f3feac3f954f34b1135c0175a98e829ac3f73
yuchenduan <116866341+yuchenduan@users.noreply.github.com>
2020-09-03 18:50:23 +0000

    Missing image

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,24 @@ class RegistrationsController < Devise::RegistrationsController
    if resource.persisted?
     if resource.active_for_authentication?
       set_flash_message! :notice, :signed_up
-   
+      sign_up(resource_name, resource)
+      respond_with resource, location: after_sign_up_path_for(resource)
+    else
+      set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+      expire_data_after_sign_in!
+      respond_with resource, location: after_inactive_sign_up_path_for(resource)
+    end
+  else
+    clean_up_passwords resource
+    set_minimum_password_length
+    response_to_sign_up_failure resource
+  end
+end
+
+private
+
+def response_to_sign_up_failure(resource)
+  if resource.email == "" && resource.password == nil
+    redirect_to root_path, alert: "Please fill in the form"
+  elsif User.pluck(:email).include? resource.email
+  
