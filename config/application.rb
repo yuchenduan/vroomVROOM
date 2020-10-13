07bf51d516c6afdf87200976e35418ee6f67bafa
yuchenduan <116866341+yuchenduan@users.noreply.github.com>
2020-10-13 21:06:22 +0000

    Updated map

--- config/application.rb
+++ config/application.rb
@@ -18,4 +18,20 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module CoinTrail
-  class Application < Rails::Applicati
+  class Application < Rails::Application
+    config.generators do |generate|
+          generate.assets false
+          generate.helper false
+          generate.test_framework  :test_unit, fixture: false
+        end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+    con
