85d45ccd84ea8d7fe467af6cdd410d976d030dac
yuchenduan <116866341+yuchenduan@users.noreply.github.com>
2020-10-22 20:32:13 +0000

    usb-storage: Add 090c:1000 to unusal-devs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,20 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = 
+  config.webpacker.check_yarn_integrity = true
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join('tmp', 'caching-dev.txt
