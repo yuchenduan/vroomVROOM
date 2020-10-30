7ef0bbc3b0c448e9c88a462f750bb47c749a0466
yuchenduan <116866341+yuchenduan@users.noreply.github.com>
2020-10-30 12:13:33 +0000

    msm: smd_pkt: Avoid prematurely stashing driver info

---
+++ config/initializers/content_security_policy.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# Define an application-wide content security policy
+# For further information see the following documentation
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+
+# Rails.application.config.content_security_policy do |policy|
+#   policy.default_src :self, :https
+#   policy.font_src    :self, :https, :data
+#   policy.img_src     :self, :https, :data
+#   policy.object_src  :none
