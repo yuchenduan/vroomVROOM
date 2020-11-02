c80777727843259a84cf91e8d7bbfd1915a4cec1
yuchenduan <116866341+yuchenduan@users.noreply.github.com>
2020-11-02 13:53:13 +0000

    Change images

---
+++ config/initializers/simple_form_bootstrap.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Please do not make direct changes to this file!
+# This generator is maintained by the community around simple_form-bootstrap:
+# https://github.com/rafaelfranca/simple_form-bootstrap
+# All future development, tests, and organization should happen there.
+# Background history: https://github.com/plataformatec/simple_form/issues/1561
+
+# Uncomment this and change the path if necessary to include your own
+# components.
+# See https://github.com/plataforma
