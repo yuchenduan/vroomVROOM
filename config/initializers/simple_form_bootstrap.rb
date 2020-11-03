a5bc6faf6cdc57cbb8a6ac3abb557b4605c1b836
yuchenduan <116866341+yuchenduan@users.noreply.github.com>
2020-11-03 13:06:37 +0000

    Fix missing imports

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -8,4 +8,20 @@
 
 # Uncomment this and change the path if necessary to include your own
 # components.
-# See https://github.com/plataforma
+# See https://github.com/plataformatec/simple_form#custom-components
+# to know more about custom components.
+# Dir[Rails.root.join('lib/components/**/*.rb')].each { |f| require f }
+
+# Use this setup block to configure all options available in SimpleForm.
+SimpleForm.setup do |config|
+  # Default class for buttons
+  config.button_class = 'btn'
+
+  # Define the default class of the input wrapper of the boolean input.
+  config.boolean_label_class = 'form-check-label'
+
+  # How the label text should be generated altogether with the required text.
+  config.label_text = lambda { |label, required, explicit_label| "#{label} #{required}" }
+
+  # Define the way to render check boxes / radio buttons with labels.
+  config.boolean_style = :i
