c5dc7f86cd666e81025c5267f0a4acf3fda1bbb7
yuchenduan <116866341+yuchenduan@users.noreply.github.com>
2020-12-23 16:27:50 +0000

    correct test

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -428,4 +428,12 @@ SimpleForm.setup do |config|
   # enable custom form wrappers
   # config.wrapper_mappings = {
   #   boolean:       :custom_boolean,
-  #   ch
+  #   check_boxes:   :custom_collection,
+  #   date:          :custom_multi_select,
+  #   datetime:      :custom_multi_select,
+  #   file:          :custom_file,
+  #   radio_buttons: :custom_collection,
+  #   range:         :custom_range,
+  #   time:          :custom_multi_select
+  # }
+end
