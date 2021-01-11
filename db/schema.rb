8cabb6102d08abdf5f07fc15507eaa756c8a8363
yuchenduan <116866341+yuchenduan@users.noreply.github.com>
2021-01-11 11:56:31 +0000

    hz

--- db/schema.rb
+++ db/schema.rb
@@ -32,4 +32,23 @@ ActiveRecord::Schema.define(version: 2018_06_10_161305) do
     t.index ["user_id"], name: "index_apis_on_user_id"
   end
 
-  create_table "exchanges", forc
+  create_table "exchanges", force: :cascade do |t|
+    t.string "base_endpoint"
+    t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
